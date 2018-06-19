1927fa47b4d931d8b6be4fcab7e76fcf608e7380
GangaSNair <40362216+GangaSNair@users.noreply.github.com>
2018-06-19 18:30:32 +0530

    Add files via upload

---
+++ Java Files/ShapeDriver.java
@@ -0,0 +1,21 @@
+public class ShapeDriver{
+	public static void main(String[] args) {
+	Circle c=new Circle();
+	Square sq=new Square(10);
+	Rectangle rc=new Rectangle();
+	
+	c.setArea();
+	c.setPerimeter();
+	c.getPerimeter();
+	c.getArea();
+	sq.setArea();
+	sq.setPerimeter();
+	sq.getPerimeter();
+	sq.getArea();
+	rc.setArea();
+	rc.setPerimeter();
+	rc.getPerimeter();
+	rc.getArea();
+	
+	}
+}
